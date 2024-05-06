fe4050c47e3b41035e3f3e42c263bcb8b81aa13d
Carlos André <carlosandre1572@gmail.com>
2024-05-06 10:12:57 -0300

    feat: add product list and product item component

---
+++ app/_components/products-list.tsx
@@ -0,0 +1,30 @@
+import { db } from "../_lib/prisma";
+import ProductItem from "./product-item";
+
+const ProductList = async () => {
+  const products = await db.product.findMany({
+    where: {
+      discountPercentage: {
+        gt: 0,
+      },
+    },
+    take: 10,
+    include: {
+      restaurant: {
+        select: {
+          name: true,
+        },
+      },
+    },
+  });
+
+  return (
+    <div className="flex gap-4 overflow-x-scroll px-5 pb-1 scrollbar-thin">
+      {products.map((product) => (
+        <ProductItem key={product.id} product={product} />
+      ))}
+    </div>
+  );
+};
+
+export default ProductList;
--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,9 @@ import Image from "next/image";
 import CategoryList from "./_components/category-list";
 import Header from "./_components/header";
 import Search from "./_components/search";
+import ProductList from "./_components/products-list";
+import { Button } from "./_components/ui/button";
+import { ChevronRightIcon } from "lucide-react";
 
 export default function Home() {
   return (
@@ -15,7 +18,7 @@ export default function Home() {
         <CategoryList />
       </div>
 
-      <div className=" pt-6">
+      <div className="space-y-4 pt-6">
         <Image
           src="/promo-banner.png"
           alt="Até 30% de desconto em pizzas"
@@ -25,6 +28,20 @@ export default function Home() {
           sizes="100vw"
           quality={100}
         />
+      </div>
+
+      <div className="space-y-4 pt-6 ">
+        <div className="flex items-center justify-between px-5">
+          <h2 className="font-semibold">Pedidos recomendados</h2>
+          <Button
+            variant="ghost"
+            className="h-fit p-0 text-primary hover:bg-transparent"
+          >
+            Ver todos
+            <ChevronRightIcon size={16} />
+          </Button>
+        </div>
+        <ProductList />
       </div>
     </>
   );
