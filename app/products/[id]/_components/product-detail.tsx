77b6946fefb0f58d35999d5df37905f118fa6b84
Carlos André <carlosandre1572@gmail.com>
2024-05-30 01:14:40 -0300

    feat: add cart button to product page

--- app/products/[id]/_components/product-detail.tsx
+++ app/products/[id]/_components/product-detail.tsx
@@ -152,6 +152,10 @@ const ProductDetail = ({
           <ProductList products={complementaryProducts} />
         </div>
       </div>
+
+      <div className="mt-6 px-5">
+        <Button className="w-full font-semibold">Adicionar à sacola</Button>
+      </div>
     </div>
   );
 };
